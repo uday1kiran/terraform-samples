f5829c1e987daa813d0a4d4dabbcdab5f1d140db
Uday Kiran Reddy <uday.r@turing.com>
2024-06-09 02:04:40 +0530

    ram,cpu,os details added

--- ec2 instance with other details/data.tf
+++ ec2 instance with other details/data.tf
@@ -16,13 +16,33 @@ data "aws_subnet" "example" {
   id       = each.value.subnet_id
 }
 
+data "aws_ami" "ami_details" {
+  for_each = data.aws_instance.this
+  owners   = ["amazon"]
+  filter {
+    name   = "image-id"
+    values = [each.value.ami]
+  }
+}
+
 locals {
   instance_details = {
     for id, instance in data.aws_instance.this :
     id => {
       instance_id        = instance.id
       instance_type      = instance.instance_type
+      instance_details   = {
+        ram_size_gb     = lookup(var.instance_types[instance.instance_type], "ram_size_gb")
+        cpu_core_count  = lookup(var.instance_types[instance.instance_type], "cpu_core_count")
+        # Add more instance type details as needed
+      }
       ami_id             = instance.ami
+      ami_details        = {
+        name            = data.aws_ami.ami_details[id].name
+        description     = data.aws_ami.ami_details[id].description
+        platform        = data.aws_ami.ami_details[id].platform_details
+        # Add more AMI details as needed
+      }
       launch_time        = "${formatdate("DD:MM:YY hh:mm:ss", timeadd(instance.launch_time, "5h30m"))} IST"
       tags               = instance.tags
       public_ip          = instance.public_ip
@@ -43,4 +63,19 @@ locals {
 
 output "instance_details" {
   value = local.instance_details
+}
+
+variable "instance_types" {
+  type = map(object({
+    ram_size_gb    = number
+    cpu_core_count = number
+    # Add more instance type attributes as needed
+  }))
+
+  default = {
+    "t2.micro"  = { ram_size_gb = 1,  cpu_core_count = 1  }
+    "t2.small"  = { ram_size_gb = 2,  cpu_core_count = 1  }
+    "t2.medium" = { ram_size_gb = 4,  cpu_core_count = 2  }
+    # Add more instance types and their details
+  }
 }
