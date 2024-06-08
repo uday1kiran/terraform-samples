040cbffdc48952df6be91a36cf6fb8caabf11918
Uday Kiran Reddy <uday.r@turing.com>
2024-06-09 01:39:22 +0530

    volumes id and size separate entries

--- ec2 instance with other details/data.tf
+++ ec2 instance with other details/data.tf
@@ -24,18 +24,20 @@ locals {
       instance_type      = instance.instance_type
       ami_id             = instance.ami
       launch_time        = instance.launch_time
+      tags               = instance.tags
       public_ip          = instance.public_ip
       private_ip         = instance.private_ip
       public_dns         = instance.public_dns
       private_dns        = instance.private_dns
       vpc_id             = data.aws_subnet.example[id].vpc_id
       subnet_id          = instance.subnet_id
-      elastic_ip         = instance.ebs_optimized ? instance.public_ip : null
+      elastic_ip         = instance.ebs_block_device[*].volume_id
       key_name           = instance.key_name
-      security_groups    = join(", ", instance.security_groups)
-      tags               = instance.tags
-      volume_ids         = join(", ", [for volume in instance.ebs_block_device : volume.volume_id])
-      volume_sizes       = join(", ", [for volume in instance.ebs_block_device : volume.volume_size])
+      security_groups    = instance.security_groups
+      root_volume_id     = tolist(instance.root_block_device)[0].volume_id
+      root_volume_size   = tolist(instance.root_block_device)[0].volume_size
+      ebs_volume_ids     = join(",", [for volume in instance.ebs_block_device : volume.volume_id])
+      ebs_volume_sizes   = join(",", [for volume in instance.ebs_block_device : volume.volume_size])
       # Add more instance attributes as needed
     }
   }
